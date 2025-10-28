ee4f43830b83f153a3159b0857bc099be39cee27
Darwin Lopez <estuardo.darwinlopez@gmail.com>
2025-10-28 21:24:02 +0000

    se arreglan datos desde vps

--- appsegura/settings.py
+++ appsegura/settings.py
@@ -129,3 +129,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = "Lax"
