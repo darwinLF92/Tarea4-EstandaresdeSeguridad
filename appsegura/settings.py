808afc58d5c5f619ee497d3912c23533cc6795e1
darwinLF92 <145072019+darwinLF92@users.noreply.github.com>
2025-10-28 16:02:58 -0600

    se corrige settings

--- appsegura/settings.py
+++ appsegura/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'appsegura.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],   # 👈 IMPORTANTE
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,9 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
-                # 👇 Nuestro context processor personalizado
-                'core.context_processors.debug_flag',
+                'core.context_processors.debug_flag',  # si lo añadiste
             ],
         },
     },
